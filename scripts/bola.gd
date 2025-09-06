ad38a9d83b3e33e249657126d97740de757b4fe8
ThiagoIFDS23 <129286938+ThiagoIFDS23@users.noreply.github.com>
2025-09-05 23:41:05 -0300

    fisica da bola implementada

--- scripts/bola.gd
+++ scripts/bola.gd
@@ -1,35 +1,20 @@
-extends Node2D
+extends RigidBody2D
 
 # Coeficientes da função quadrática
-@export var a: float = -0.02
-@export var b: int = 0.0
-@export var c: int = 0.0
+@export var a: int = -1
+@export var b: int = 0
+@export var c: int = 2
 
-# Velocidade no eixo X
-@export var speed: float = 130.0
-
-# Origem personalizada
-@export var origin: Vector2 = Vector2(960, 624)
-
-# Posição no "sistema de coordenadas matemático"
-var x_pos: float = 0.0
 var launched = false
-
+var originX = 744.0
+var originY = 506.0
 
 func launch() -> void:
 	launched = true
 
-func _process(delta: float) -> void:
+func _physics_process(delta: float) -> void:
 	if launched:
-	# Avança no eixo X (sistema matemático)
-		x_pos += speed * delta
-		# Calcula Y pela função quadrática
-		var y_math = a * pow(x_pos, 2) + b * x_pos + c
-
-		
-		# Converte para coordenadas de tela
-		var screen_x = origin.x + x_pos*1.16483516 #diferença que tem de um eixo p o outro
-		var screen_y = origin.y - y_math  # inverte porque no Godot o Y cresce para baixo
-		
-		# Atualiza posição da bola
-		position = Vector2(screen_x, screen_y)
+		linear_velocity.x = 200.0
+		var x = (global_position.x - originX) / (22.50)
+		position.y = ((a*pow(x,2)+b*x+c) * 22.50)*(-1) + originY
+		print(position)
--- scripts/target.gd
+++ scripts/target.gd
@@ -1,7 +1,7 @@
 extends Area2D
+var score = 0
 
-
-
-
-func _on_body_entered(body: Node2D) -> void:
+func _on_body_entered(_body: Node2D) -> void:
 	queue_free()
+	#score += 1
+	#$"../ScoreLabel".text = str(score)
