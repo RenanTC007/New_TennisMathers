3a1b549ee97e369712d8ca86b59c7477c74a683f
ThiagoIFDS23 <129286938+ThiagoIFDS23@users.noreply.github.com>
2025-09-06 11:19:38 -0300

    diminuicao da bola e dos alvos

--- scripts/bola.gd
+++ scripts/bola.gd
@@ -2,12 +2,13 @@ extends RigidBody2D
 
 # Coeficientes da função quadrática
 @export var a: int = -1
-@export var b: int = 0
-@export var c: int = 2
+@export var b: int = 2
+@export var c: int = 10
 
 var launched = false
 var originX = 744.0
 var originY = 506.0
+
 
 func launch() -> void:
 	launched = true
@@ -15,6 +16,5 @@ func launch() -> void:
 func _physics_process(delta: float) -> void:
 	if launched:
 		linear_velocity.x = 200.0
-		var x = (global_position.x - originX) / (41.0)
+		var x = (global_position.x - originX) / (82.0)
 		position.y = ((a*pow(x,2)+b*x+c) * 82.0)*(-1) + originY
-		print(position)
