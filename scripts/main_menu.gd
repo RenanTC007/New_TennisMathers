f18973998d06a421bb2c86258b810b2abe479334
RenanTC007 <nantrajano@gmail.com>
2025-09-08 21:37:19 -0300

    sem cena de tutorial

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -2,16 +2,16 @@ extends Control
 		
 func _ready():
 	$Jogar.connect("pressed", Callable(self, "_on_play_pressed"))
-	$Ajuda.connect("pressed", Callable(self, "_on_tutorial_pressed"))
+	#$Ajuda.connect("pressed", Callable(self, "_on_tutorial_pressed"))
 	$Sair.connect("pressed", Callable(self, "_on_quit_pressed"))
 	
 
 func _on_play_pressed():
 	get_tree().change_scene_to_file("res://scenes/main.tscn")
 
-func _on_tutorial_pressed():
-	# Exibe uma tela de instruções
-	get_tree().change_scene_to_file("res://scenes/tutorial.tscn")
+#func _on_tutorial_pressed():
+	## Exibe uma tela de instruções
+	#get_tree().change_scene_to_file("res://scenes/tutorial.tscn")
 
 func _on_quit_pressed():
 	# Sai do jogo
